92ad0ae8ce653f49e02be24ccfe9c996046dbcdc
AnkitKumar4 <krankit0204@gmail.com>
2024-02-19 22:32:07 +0530

    table of numbers

---
+++ table.java
@@ -0,0 +1,17 @@
+package testing;
+
+import java.util.*;
+class table {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int sum=0;
+       for(int i=1;i<=10;i++){
+           sum=n*i;
+           System.out.println(sum);
+           
+       }
+    }
+    
+    
+}
